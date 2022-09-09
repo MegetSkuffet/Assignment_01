de75dff004515888b84577bc0f577643f4b0e8fc
luel <luel@itu.dk>
2022-09-09 14:01:18 +0200

    Tested filter

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -4,9 +4,21 @@ public class IteratorsTests
 {
     // Test Iterators.cs
     [Fact]
-    public void Flatten_Given_123_123_123_should_return_123123123(){
-        var list = new List<List<int>> {new List<int> {1, 2, 3},new List<int> {1, 2, 3},new List<int> {1,2,3}};
+    public void Flatten_Given_123_123_123_should_return_123123123()
+    {
+        var list = new List<List<int>> { new List<int> { 1, 2, 3 }, new List<int> { 1, 2, 3 }, new List<int> { 1, 2, 3 } };
         var flattened = Iterators.Flatten(list);
-        Assert.Equal(new List<int> {1,2,3,1,2,3,1,2,3}, flattened);
+        Assert.Equal(new List<int> { 1, 2, 3, 1, 2, 3, 1, 2, 3 }, flattened);
+    }
+
+    [Fact]
+    //Test that the list is filtered correctly given predicate function
+    public void Filter_Given_123_should_return_2()
+    {
+        Predicate<int> even = Even;
+        bool Even(int i) => i % 2 == 0;
+        var list = new List<int> { 1, 2, 3, };
+        var filtered = Iterators.Filter(list, even);
+        Assert.Equal(new List<int> { 2 }, filtered);
     }
 }
