435d9bf07905daf66a4ca622fb939c800c39e729
luel <luel@itu.dk>
2022-09-09 13:54:36 +0200

    Implemented flatten and filter, tested flatten

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -2,4 +2,11 @@ namespace Assignment1.Tests;
 
 public class IteratorsTests
 {
+    // Test Iterators.cs
+    [Fact]
+    public void Flatten_Given_123_123_123_should_return_123123123(){
+        var list = new List<List<int>> {new List<int> {1, 2, 3},new List<int> {1, 2, 3},new List<int> {1,2,3}};
+        var flattened = Iterators.Flatten(list);
+        Assert.Equal(new List<int> {1,2,3,1,2,3,1,2,3}, flattened);
+    }
 }
--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -2,7 +2,22 @@ namespace Assignment1;
 
 public static class Iterators
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
+    // Flatten takes as argument a stream of a stream of T's. It should return a stream of T's.
+    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items){
+        foreach(var item in items){
+            foreach(var i in item){
+                yield return i;
+            }
+        }
+    } 
 
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
+    // Filter takes as arguments a stream of T's and a function which returns true or false when applied to an instance of T. 
+    // It returns a stream of only the Ts where the predicate returns true.
+    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) {
+        foreach(var item in items){
+            if(predicate(item)){
+                yield return item;
+            }
+        }
+    }
 }
