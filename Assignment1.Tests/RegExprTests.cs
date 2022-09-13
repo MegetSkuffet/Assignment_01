9aa52a41b34efacd11179e9067695852ac5a8712
Malte Alexander Koefoed Schalck <malsc@itu.dk>
2022-09-13 13:07:10 +0200

    regex splitlines

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -2,4 +2,13 @@ namespace Assignment1.Tests;
 
 public class RegExprTests
 {
+     // Test regex.cs
+    [Fact]
+    public void splitline_given_hejmeddig_returns_hej_med_dig(){
+        var list = new List<string>{"hej med dig"};
+        var result = RegExpr.SplitLine(list);
+
+       
+        Assert.Equal(new List<string> {"hej","med","dig"}, result);
+    }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,8 +1,26 @@
 namespace Assignment1;
+using System.Text.RegularExpressions;
 
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+    {
+        var pattern = @"\w+";
+        foreach (var line in lines)
+        {
+            var matches = Regex.Matches(line, pattern);
+            foreach (Match word in matches)
+            {
+                yield return word.Value;
+            }
+        }
+
+         
+        /*foreach(string line in lines) {
+            var words = Regex.Split(line, @" ").ToList();
+            foreach(var word in words) yield return word;
+        }*/
+    }
 
     public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
 
